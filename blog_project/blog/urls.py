b2c6a49240df77e690b9f05dbaf554fbaf91076b
Setas8 <dcsetas@gmail.com>
2024-05-21 08:20:49 +0200

    add create and delete blog

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/delete/<int:pk>/', views.post_delete, name='post_delete'),
 ]
